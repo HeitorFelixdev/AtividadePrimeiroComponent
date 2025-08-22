4074a39b27d9dbe0b26e3de0892f36e7784f670c
José Heitor Felix Guimarães <jose.00000855161@unicap.br>
2025-08-22 02:37:10 +0000

    Primeiro componente React - Profile

---
+++ meu-site/app/Profile.js
@@ -0,0 +1,14 @@
+export default function Profile() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <img 
+        src="/foto.jpg" 
+        alt="Minha foto" 
+        style={{ width: "150px", borderRadius: "50%" }}
+      />
+      <h2>José Heitor</h2>
+      <p>Sou estudante de Sistemas de Informação e apaixonado por tecnologia.  
+         Gosto de aprender programação e criar projetos que possam ajudar pessoas.</p>
+    </div>
+  );
+}
---
+++ meu-site/app/page.js
@@ -0,0 +1,10 @@
+import Profile from "./Profile";
+
+export default function Home() {
+  return (
+    <main style={{ textAlign: "center", padding: "20px" }}>
+      <h1>Site criado para a disciplina Programação Front-End</h1>
+      <Profile />
+    </main>
+  );
+}
